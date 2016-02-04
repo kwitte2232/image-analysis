270a60bf887d1992cabf0a2d08cd9d73fba20357
kwitte2232 <kwitte@uchicago.edu>
2016-02-04 11:23:54 -0600

    working on functions to extract metadata info

--- Text_Manipulation_Scripts/Extract_Text_Data.py
+++ Text_Manipulation_Scripts/Extract_Text_Data.py
@@ -32,16 +32,46 @@ def open_metadata(metadata, info, num_items):
         imp_data = []
 
         regex = '.*' + info
-        print(regex)
 
         for line in reader:
             if data_found:
                 if len(imp_data) <= num_items:
-                #while len(imp_data) <= num_items:
                     imp_data.append(line)
             elif re.search(regex, line):
-                #print("worked!")
                 data_found = True
                 imp_data.append(line)
 
         return imp_data
+
+def extract_data(imp_data, specific_info):
+    '''
+    Extracts the desired data from a list
+
+    Inputs:
+        imp_data: list of strings containing the image info that you wanted
+        specific_info: string, what data you want out of imp_data
+            ** Will convert to list of strings
+    Returns:
+        string, string with the desired info that you wanted
+    '''
+
+    regex = ".*" + specific_info
+
+    for data in imp_data:
+        if re.search(regex, data):
+            data_split = data.split(" - ")
+            important_info = data_split[1]
+            desired_info = ""
+            for char in important_info:
+                if char.isdigit() or char == '.':
+                    desired_info = desired_info + char
+
+    desired_info = desired_info + '_(s)'
+    return desired_info
+
+
+
+
+def rename_folders(directory):
+    '''
+    '''
